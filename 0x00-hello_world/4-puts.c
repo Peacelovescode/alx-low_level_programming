6e699a13cf0fd77d97646d538a5da3eb6af94b67
“Agu <agupeace2003@gmail.com>
2022-12-08 18:26:48 -0800

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Sucess)
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
@@ -11,3 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
