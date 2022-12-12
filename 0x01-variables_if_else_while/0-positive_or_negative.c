cc7e7e24305309081ebbd2ecc8c7ff9861b683e8
“Agu <agupeace2003@gmail.com>
2022-12-12 04:06:04 -0800

    positive and negative words

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,31 +2,27 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return - Always 0 (Success)
  */
 
-int main(void);
-
+int main(void)
 {
 
-int n;
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
-	{
-	
-	if (n < 0)
-		printf("%d\n, is positive", n);
-
 	if (n > 0)
-		printf("%d\n, is negative", n);
-
-	if (n == 0)
-		printf("%d\n, is zero", n);
-	}
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 	return (0);
-
 }
