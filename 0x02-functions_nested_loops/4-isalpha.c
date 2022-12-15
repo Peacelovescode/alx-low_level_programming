8861b03cc4d7dc911ba5e5e4d46b87a896f61bf8
“Agu <agupeace2003@gmail.com>
2022-12-14 21:56:33 -0800

    changes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isaplha - checks for only alphabets
+ * _isalpha - checks for only alphabets
  * @c: character to be checked
  *
  * Return: 1 if character is a letter if not 0
@@ -9,7 +9,7 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 			return (1);
 		else
 			return (0);
