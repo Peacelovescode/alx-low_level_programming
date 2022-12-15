74fd9b695b664a271795fb4827b401a695c86202
“Agu <agupeace2003@gmail.com>
2022-12-14 23:42:55 -0800

    changes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * gt: The integer to be computed
+ * Return:The absolute value 
+ */
+
+int _abs(int) 
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
