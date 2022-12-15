8c2d95e88c9965f6d84fa2f430c0837fa577de87
“Agu <agupeace2003@gmail.com>
2022-12-14 21:27:36 -0800

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,14 @@
 #include "main.h"
 /**
- *
+ *islower - checks only lowercase
+ *@c: the character
+ * Return: 1 if letter is lowercase if not always 0
  */
 
 int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
