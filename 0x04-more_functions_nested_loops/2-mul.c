2fc5ca6d618b9e6ba30841a3d7b2c7bd6d663e63
“Agu <agupeace2003@gmail.com>
2022-12-16 07:44:11 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies a two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
