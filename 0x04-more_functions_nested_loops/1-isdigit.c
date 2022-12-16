f5a9410c7c861d50f4c01019bfb370b59ad5f449
“Agu <agupeace2003@gmail.com>
2022-12-16 07:35:21 -0800

    chages to task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,17 +2,13 @@
 /**
  * _isdigit -  checks for digits in a string
  * @c: input character
+ *
  * Return: 1 is c is a digit and 0 otherwise
  */
-
 int _isdigit(int c)
 {
-	if (c <= 0 && c >= 9)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c <= 0 && c >= 9)
+return (1);
+else
+return (0);
 }
