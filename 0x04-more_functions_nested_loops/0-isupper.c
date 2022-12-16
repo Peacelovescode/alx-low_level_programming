0561276d137c259a1fe03b749bc2f89e4ac69eb2
“Agu <agupeace2003@gmail.com>
2022-12-16 07:10:15 -0800

    changes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,15 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z' ; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 1;
+break;
+}
+}
+return (isupper);
 }
