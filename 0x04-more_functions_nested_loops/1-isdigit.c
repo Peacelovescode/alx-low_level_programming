48c931b0e2734bf89363b994cdc980aee0f0a679
“Agu <agupeace2003@gmail.com>
2022-12-16 07:31:47 -0800

    print only digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit -  checks for digits in a string
+ * @c: input character
+ * Return: 1 is c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 0 && c >= 9, c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
