0466a72463cced03b557440d60a04d9f552bde62
Agu <agupeace2003@gmail.com>
2022-12-19 17:41:49 +0000

    Task 3 and main.h files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * int _strlen - calculate the length of a string
+ * @s: is a character
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+
+	while (*s != '\0')
+	{
+		len ++;
+		s++;
+	}
+
+}
