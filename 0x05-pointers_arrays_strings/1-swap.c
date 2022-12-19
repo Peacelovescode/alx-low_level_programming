aa46f84eb5e8f9658875b6cc4191c84f6d1618da
Agu <agupeace2003@gmail.com>
2022-12-19 17:01:13 +0000

    task 2 and main.h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Swap the value of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a = 42;
+	*b = 98;
+}
