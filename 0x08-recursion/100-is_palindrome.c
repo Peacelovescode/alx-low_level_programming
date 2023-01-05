9db358c46a4b97b62316bab7174c5903fd1215db
Agu <agupeace2003@gmail.com>
2023-01-05 13:58:13 +0000

    changes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,6 +7,7 @@ int is_palindrome(char *s);
 /**
 * find_strlen - Returns the length of a string
 * @s: the string to be measured
+*
 * Return: the length of the string
 */
 int find_strlen(char *s)
@@ -14,4 +15,4 @@ int find_strlen(char *s)
 	int len = 0;
 
 	if (*(s + len))
-	{ 
+	{
