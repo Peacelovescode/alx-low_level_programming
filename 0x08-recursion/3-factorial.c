225b132bf2b3c5cda3c852d16d10e10a4c470177
Agu <agupeace2003@gmail.com>
2023-01-05 10:24:14 +0000

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main."
+/**
+* factoorial - process the factorial of a given integer
+* @n: the integer to find its factorial
+* return
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	if (n <= 1)
+	return (1);
+	else
+	{
+		return (n * factorial(n-1);
+	}
+}
